f2775acddab52e9d4d33c85230dea045df7da8db
sunjianan <sunjianan2021@ia.ac.cn>
2023-11-16 19:59:16 +0800

    fix: correct calculation of FLOPS.

--- src/main.cpp
+++ src/main.cpp
@@ -61,8 +61,8 @@ int main(int argc, char** argv)
     cout << duration.count() << endl;
 
     double num_flop = 2.0 * size * size * size;
-    double flops = num_flop / duration.count() * 1000;
-    cout << "openblas with size " << size << " flops (flops): ";
+    double flops = num_flop / duration.count() * 1e6;
+    cout << "openblas with size " << size << " performance (FLOPS): ";
     cout << flops << endl << endl << endl;
 
     return 0;
